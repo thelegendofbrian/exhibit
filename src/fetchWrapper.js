a8016df7c9f67c90fe49bbc9f735c059cebffab3
Travis Bruce <travis.bruce1994@gmail.com>
2019-11-10 00:25:14 -0600

    fix formatting

--- src/fetchWrapper.js
+++ src/fetchWrapper.js
@@ -2,14 +2,14 @@
 // import 'whatwg-fetch';
 
 function fetchWrapper() {
-	
-	if (window.origin === 'http://localhost:3000') {
-		// dev env
-		arguments[0] = 'http://localhost' + arguments[0];
-    }
-    // not sure if necessary since no csrf
-	// arguments[1].credentials = 'include';
-	return fetch.apply(this, arguments);
+
+  if (window.origin === 'http://localhost:3000') {
+    // dev env
+    arguments[0] = 'http://localhost' + arguments[0];
+  }
+  // not sure if necessary since no csrf
+  // arguments[1].credentials = 'include';
+  return fetch.apply(this, arguments);
 }
 
 export default fetchWrapper;
