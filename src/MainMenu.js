ac0740716a53f6785c1eb06408e529d6772ec0c0
Brian Cook <thelegendofbrian@gmail.com>
2019-11-16 17:24:07 -0800

    Added modal about page placeholder to menu

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Menu, Icon, Modal } from 'semantic-ui-react';
+import { Menu, Icon, Modal, Placeholder, Reveal } from 'semantic-ui-react';
 // import './MainMenu.css';
 
 class MainMenu extends React.Component {
@@ -27,12 +27,50 @@ class MainMenu extends React.Component {
       />
     )
 
+    const exhibitIcon = (
+      <Modal
+        trigger={
+          <Menu.Item>
+          <Reveal animated='fade'>
+            <Reveal.Content visible>
+              <img alt='Logo' src='/logo.svg' />
+            </Reveal.Content>
+            <Reveal.Content hidden>
+              <Icon name='question circle' color='green' fitted style={{ fontSize: '3em' }} />
+            </Reveal.Content>
+          </Reveal>
+          </Menu.Item>
+        }
+        header='About Exhibit'
+        content={
+          <Placeholder fluid>
+            <Placeholder.Paragraph>
+              <Placeholder.Line />
+              <Placeholder.Line />
+              <Placeholder.Line />
+              <Placeholder.Line />
+              <Placeholder.Line />
+              <Placeholder.Line />
+              <Placeholder.Line />
+              <Placeholder.Line />
+              <Placeholder.Line />
+              <Placeholder.Line />
+              <Placeholder.Line />
+              <Placeholder.Line />
+              <Placeholder.Line />
+            </Placeholder.Paragraph>
+          </Placeholder>
+        }
+        size='large'
+        actions={['Oh okay. Cool story.']}
+      />
+    )
+
     return (
       <Menu>
         <Menu.Item className='menuPadLeft' />
-        <Menu.Item>
-          <img alt='Logo' src='/logo.svg' />
-        </Menu.Item>
+
+        {exhibitIcon}
 
         <Menu.Item
           name='userStats'
