e6d7ad987947bbffe149c5a952c4a5fca1bd880a
Travis Bruce <travis.bruce1994@gmail.com>
2020-01-12 21:41:11 -0600

    change portConfig to a soft dependency

--- src/fetchWrapper.js
+++ src/fetchWrapper.js
@@ -1,18 +1,23 @@
 // Optional polyfill
-// import 'whatwg-fetch';
-import portCfg from './portConfig';
+// import 'whatwg-fetch'
 
 function fetchWrapper() {
 
-  if (window.origin === `http://localhost:${portCfg.devClient}`) {
-    // dev env
-    arguments[0] = `http://localhost:${portCfg.devServer}${arguments[0]}`;
-  }
   if (!arguments[1]) {
-    arguments[1] = {};
+    arguments[1] = {}
   }
-  arguments[1].credentials = 'include';
-  return fetch.apply(this, arguments);
+  arguments[1].credentials = 'include'
+
+  if (window.origin.indexOf('localhost') !== -1 || window.origin.indexOf('127.0.0.1') !== -1) {
+    // dev env
+
+    return import('./portConfig.js').then(module => {
+      arguments[0] = `http://localhost:${module.default.devServer}${arguments[0]}`
+      return fetch.apply(this, arguments)
+    })
+  }
+
+  return fetch.apply(this, arguments)
 }
 
-export default fetchWrapper;
+export default fetchWrapper
