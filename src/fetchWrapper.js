3f9d4de51502d145a3b3d43705a21205408710f5
Travis Bruce <travis.bruce1994@gmail.com>
2020-01-19 11:27:06 -0600

    Add ability to debug date when in non-prod environments.

--- src/fetchWrapper.js
+++ src/fetchWrapper.js
@@ -11,6 +11,14 @@ function fetchWrapper() {
   if (window.origin.indexOf('localhost') !== -1 || window.origin.indexOf('127.0.0.1') !== -1) {
     // dev env
 
+    let date = window.location.search;
+    date = date.substring(date.indexOf('=') + 1);
+
+    if (date.length === 10) {
+      console.log('fetchWrapper: sending custom date: ' + date)
+      document.cookie = `debugDate=${date}`
+    }
+
     return import('./portConfig.js').then(module => {
       arguments[0] = `http://localhost:${module.default.devServer}${arguments[0]}`
       return fetch.apply(this, arguments)
